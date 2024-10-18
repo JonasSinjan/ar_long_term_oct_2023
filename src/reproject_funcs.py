860aeed8235edcde70e5caff1fc1426e21455d65
JonasSinjan <sinjan@mps.mpg.de>
2024-10-18 12:01:24 +0200

    add mu functions

--- src/reproject_funcs.py
+++ src/reproject_funcs.py
@@ -5,7 +5,7 @@ import sunpy.map
 from datetime import datetime as dt
 import astropy.units as u
 
-from .stereo_help import image_register
+from stereo_help import image_register
 
 from astropy.wcs import WCS
 from reproject import reproject_adaptive
@@ -247,3 +247,244 @@ def get_hmi_hrt_aligned(hrt_file,hmi_file,err:tuple = None):
         err=get_hrt_wcs_err(hrt_file,hmi_file)
     hrt_remap, hmi_map = get_hrt_remapped_on_hmi(hrt_file, hmi_file, err)
     return [hrt_remap,hmi_map]
+
+
+def mu_angle_arr(hdr,coord=None):
+    """
+    input
+    hdr: header or filename
+    coord: pixel for which the mu angle is found (if None: center of the FoV)
+    
+    output
+    mu = cosine of the heliocentric angle
+    """
+    if type(hdr) is str:
+        hdr = fits.getheader(hdr)
+    
+    center=center_coord(hdr)
+    Rpix=(hdr['RSUN_ARC']/hdr['CDELT1'])
+    xlen=int(hdr['NAXIS1'])
+    ylen=int(hdr['NAXIS2'])
+    x = range(xlen)
+    y = range(ylen)
+    xv, yv = np.meshgrid(x, y)
+    coord = np.ones((len(xv.flatten()),2))
+    coord[:,0] = xv.flatten()
+    coord[:,1] = yv.flatten()
+
+    if coord is None:
+        coord = np.asarray([(hdr['PXEND1']-hdr['PXBEG1'])/2,
+                            (hdr['PXEND2']-hdr['PXBEG2'])/2])
+    else:
+        coord = np.asarray(coord,dtype=float)
+    
+    coord_tmp = coord - center[:2]
+    mu = np.sqrt(Rpix**2 - (coord_tmp[:,0]**2 + coord_tmp[:,1]**2)) / Rpix
+    return mu
+
+
+def center_coord(hdr):
+    """calculate the center of the solar disk in the rotated reference system
+    Parameters
+    ----------
+    hdr : header
+        header of the fits file
+    Returns
+    -------
+    center: [x,y,1] coordinates of the solar disk center (units: pixel)
+    """
+    pxbeg1 = hdr['PXBEG1']
+    pxend1 = hdr['PXEND1']
+    pxbeg2 = hdr['PXBEG2']
+    pxend2 = hdr['PXEND2']
+    coord=np.asarray([hdr['CRPIX1'],
+            hdr['CRPIX2'],
+           1])
+
+    angle = hdr['CROTA'] # positive angle = clock-wise rotation of the reference system axes 
+    rad = angle * np.pi/180
+    rot = np.asarray([[np.cos(rad),-np.sin(rad),0],[np.sin(rad),np.cos(rad),0],[0,0,1]])
+    rc = [(pxend1-pxbeg1)/2,(pxend2-pxbeg2)/2] # CRPIX from 1 to 2048, so 1024.5 is the center
+
+    tr = np.asarray([[1,0,rc[0]],[0,1,rc[1]],[0,0,1]])
+    invtr = np.asarray([[1,0,-rc[0]],[0,1,-rc[1]],[0,0,1]])
+    M = tr @ rot @ invtr
+
+    coord = (M @ coord)[:2]
+
+    # center of the sun in the rotated reference system
+    center=np.asarray([coord[0]-hdr['CRVAL1']/hdr['CDELT1']-1,
+                       coord[1]-hdr['CRVAL2']/hdr['CDELT2']-1,
+                       1])
+    # rotation of the sun center back to the original reference system
+    angle = -hdr['CROTA'] # positive angle = clock-wise rotation of the reference system axes 
+    rad = angle * np.pi/180
+    rot = np.asarray([[np.cos(rad),-np.sin(rad),0],[np.sin(rad),np.cos(rad),0],[0,0,1]])
+    
+    tr = np.asarray([[1,0,rc[0]],[0,1,rc[1]],[0,0,1]])
+    invtr = np.asarray([[1,0,-rc[0]],[0,1,-rc[1]],[0,0,1]])
+    M = tr @ rot @ invtr
+
+    center = (M @ center)
+    
+    return center
+
+
+def mu_angle_arr_hmi(hdr,coord):
+    """
+    input
+    hdr: header or filename
+    coord: pixel for which the mu angle is found
+    
+    output
+    mu = cosine of the heliocentric angle
+    """
+    if type(hdr) is str:
+        hdr = fits.getheader(hdr)
+    
+    center=center_coord_hmi(hdr)
+    Rpix=(hdr['RSUN_OBS']/hdr['CDELT1'])
+    
+    coord = np.asarray(coord,dtype=float)
+    
+    coord_tmp = coord - center[:2]
+    mu = np.sqrt(Rpix**2 - (coord_tmp[:,0]**2 + coord_tmp[:,1]**2)) / Rpix
+    return mu
+
+
+def center_coord_hmi(hdr):
+    """calculate the center of the solar disk in the rotated reference system
+    Parameters
+    ----------
+    hdr : header
+        header of the fits file
+    Returns
+    -------
+    center: [x,y,1] coordinates of the solar disk center (units: pixel)
+    """
+    pxbeg1 = 0
+    pxend1 = hdr['NAXIS1']-1
+    pxbeg2 = 0
+    pxend2 = hdr['NAXIS1']-1
+    coord=np.asarray([hdr['CRPIX1'],
+            hdr['CRPIX2'],
+           1])
+
+    hdr['CROTA2'] = 0
+    angle = hdr['CROTA2'] # positive angle = clock-wise rotation of the reference system axes 
+    rad = angle * np.pi/180
+    rot = np.asarray([[np.cos(rad),-np.sin(rad),0],[np.sin(rad),np.cos(rad),0],[0,0,1]])
+    rc = [(pxend1-pxbeg1)/2,(pxend2-pxbeg2)/2] # CRPIX from 1 to 2048, so 1024.5 is the center
+
+    tr = np.asarray([[1,0,rc[0]],[0,1,rc[1]],[0,0,1]])
+    invtr = np.asarray([[1,0,-rc[0]],[0,1,-rc[1]],[0,0,1]])
+    M = tr @ rot @ invtr
+
+    coord = (M @ coord)[:2]
+
+    # center of the sun in the rotated reference system
+    center=np.asarray([coord[0]-hdr['CRVAL1']/hdr['CDELT1']-1,
+                       coord[1]-hdr['CRVAL2']/hdr['CDELT2']-1,
+                       1])
+    # rotation of the sun center back to the original reference system
+    angle = -hdr['CROTA2'] # positive angle = clock-wise rotation of the reference system axes 
+    rad = angle * np.pi/180
+    rot = np.asarray([[np.cos(rad),-np.sin(rad),0],[np.sin(rad),np.cos(rad),0],[0,0,1]])
+    
+    tr = np.asarray([[1,0,rc[0]],[0,1,rc[1]],[0,0,1]])
+    invtr = np.asarray([[1,0,-rc[0]],[0,1,-rc[1]],[0,0,1]])
+    M = tr @ rot @ invtr
+
+    center = (M @ center)
+    
+    return center
+
+
+def get_hrt_MU_remapped_on_hmi(hrt_file, hmi_file, err):
+    """remap HRT MU to HMI blos map coords with HMI pixel size, masking out the field stop region/apodization areas if PHI map is larger than (1800,1800)
+
+    Parameters
+    ----------
+    hrt_file : str
+        HRT blos map path
+    hmi_file : str
+        HMI blos map path
+    err : tuple
+        CRPIX1 and CRPIX2 error in Skycoords
+    
+    Returns
+    -------
+    hrt_remap : sunpy.map.Map
+        HRT blos map remapped to HMI coords
+    hmi_map : sunpy.map.Map
+        HMI blos map
+    """
+    print(dt.now())
+
+    errx=err[0]
+    erry=err[1]
+    h = fits.getheader(hrt_file)
+    h['CRVAL1']=h['CRVAL1']-errx
+    h['CRVAL2']=h['CRVAL2']-erry
+    tmp=mu_angle_arr(h).reshape(fits.getdata(hrt_file).shape)
+    arr=np.zeros(tmp.shape)
+    if tmp.shape[0]<=1800 and tmp.shape[1]<=1800:
+        arr=tmp
+    elif tmp.shape[0]>1800 and tmp.shape[1]>1800:
+        arr[150:-150,150:-150]=tmp[150:-150,150:-150]
+    hrt_mu_map = sunpy.map.Map(arr,h)
+    hmi_map=sunpy.map.Map(hmi_file).rotate()
+
+    out_header = sunpy.map.make_fitswcs_header(
+         hmi_map.data.shape, hmi_map.reference_coordinate.replicate(rsun=hrt_mu_map.reference_coordinate.rsun),
+         scale=u.Quantity(hmi_map.scale),
+         instrument="SO/PHI-HRT",
+         observatory="SolO",
+         wavelength=hrt_mu_map.wavelength
+         )
+    out_header['dsun_obs'] = hmi_map.coordinate_frame.observer.radius.to(u.m).value
+    out_header['hglt_obs'] = hmi_map.coordinate_frame.observer.lat.value
+    out_header['hgln_obs'] = hmi_map.coordinate_frame.observer.lon.value
+    
+    out_header['crpix1'] = hmi_map.fits_header['CRPIX1']
+    out_header['crpix2'] = hmi_map.fits_header['CRPIX2']
+    out_header['crval1'] = hmi_map.fits_header['CRVAL1']
+    out_header['crval2'] = hmi_map.fits_header['CRVAL2']    
+    out_header['PC1_1'] = hmi_map.fits_header['PC1_1']
+    out_header['PC1_2'] = hmi_map.fits_header['PC1_2']
+    out_header['PC2_1'] = hmi_map.fits_header['PC2_1']
+    out_header['PC2_2'] = hmi_map.fits_header['PC2_2']
+    out_header['cdelt1'] = hmi_map.fits_header['cdelt1']
+    out_header['cdelt2'] = hmi_map.fits_header['cdelt2']
+    out_WCS=WCS(out_header)
+
+    hrt_repro, footprint = reproject_adaptive(hrt_mu_map, out_WCS, hmi_map.data.shape)
+    
+    #print(tmp.shape)
+    
+    if tmp.shape[0]<=1800 and tmp.shape[1]<=1800:
+        hrt_mu_map = sunpy.map.Map((hrt_repro, hmi_map.meta))
+    elif tmp.shape[0]>1800 and tmp.shape[1]>1800:
+        arr_mask=np.zeros(tmp.shape)
+        arr_mask[150:-150,150:-150]=1
+        mask_map = sunpy.map.Map(arr_mask,h)
+        mask_hrt,_= reproject_adaptive(mask_map, out_WCS, hmi_map.data.shape)
+        mask_hrt[mask_hrt<1]=np.nan
+        hrt_mu_map = sunpy.map.Map((hrt_repro*mask_hrt,hmi_map.meta))
+
+    hmi_shape = hmi_map.data.shape
+    x = range(hmi_shape[0])
+    y = range(hmi_shape[1])
+    xv, yv = np.meshgrid(x, y)
+    hmi_coords = np.ones((len(xv.flatten()),2))
+    hmi_coords[:,0] = xv.flatten()
+    hmi_coords[:,1] = yv.flatten()
+    hmi_mu = mu_angle_arr_hmi(hmi_map.fits_header, hmi_coords)
+    hmi_mu = hmi_mu.reshape(hmi_shape)
+    if tmp.shape[0]>1800 and tmp.shape[1]>1800:
+        hmi_mu[np.where(np.isnan(mask_hrt))] = np.nan
+    hmi_mu_map = sunpy.map.Map((hmi_mu,hmi_map.meta))
+    print(dt.now())
+    
+    return hrt_mu_map, hmi_mu_map
+
